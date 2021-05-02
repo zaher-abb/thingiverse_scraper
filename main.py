bf34af4f4a15a947ec537f1ae33387071b031019
zaher-abb <abboudz@th-brandenburg.de>
2021-05-02 23:35:57 +0200

    commit

--- main.py
+++ main.py
@@ -1,62 +1,88 @@
 from selenium import webdriver
 from time import sleep
+import json
+
+class Product :
+
+    # TODO : persons who write comments and the comments
+    def __init__(self, name,owner, summary):
+        self.owner = owner
+        self.name = name
+        self.summary = summary
+
+
+
+
+
+
 
 driver = webdriver.Chrome("D:\ChromDriver\chromedriver_win32\chromedriver.exe")
+
 driver.get('https://www.thingiverse.com/')
 sleep(3)
+
+
 categorySort__dropdown = driver.find_element_by_xpath(
     "//button[contains(@class,'CategorySort__dropdownButton--gpHIi Dropdown__dropdownButton--1iEp1')]").click()
 categorySort__dropdown_select_3d_Printing = driver.find_element_by_xpath("//span[text()='3D Printing']").click()
 sleep(3)
 
-url_list=[]
+url_list = []
 
-result=driver.find_elements_by_class_name('ThingCardBody__cardBodyWrapper--ba5pu')
+condition = True
+testTemp=0
+while condition:
 
+    result = driver.find_elements_by_class_name('ThingCardBody__cardBodyWrapper--ba5pu')
 
-for i in range(len(result)):
+    for i in range(len(result)):
+        try:
+            url_list.append(result[i].get_property('href'))
+        except Exception as  e:
+            print(e)
     try:
-     url_list.append(result[i].get_property('href'))
-    except:
-        print("continue")
+        driver.find_element_by_xpath("//div[@class='Pagination__button--2X-2z Pagination__more--24exV']").click()
 
+        testTemp+=1
+        if testTemp >=1 :
+            break
+        sleep(3)
+    except Exception as e:
+        condition = False
+        print(e)
 
 
 print(url_list)
-# sleep(5)
-# list_product = driver.find_elements_by_class_name("SearchResult__searchResultItem--c4VZk")
-# for product in range(1,len(list_product)):
-#     driver.find_element_by_xpath(
-#         "//a[@href='https://www.thingiverse.com/thing:4813609']//img[{product}]".format(product=product)).click()
-#     sleep(5)
-#     driver.execute_script("window.history.go(-1)")
-#     sleep(7)
+
+list_of_products=[]
+for i in url_list[:5] :
+    try :
+        driver.get(i)
+        sleep(5)
+        # name
+        name = driver.find_element_by_class_name('ThingPage__modelName--3CMsV').text
+        # owner
+        owner = driver.find_element_by_xpath(
+            '//*[@id="react-app"]/div/div/div/div[5]/div[1]/div/div[1]/div/div[2]/a').get_attribute('href')
+        # Summary
+        summary = driver.find_element_by_xpath("//div[@class='ThingPage__description--14TtH']//p[1]").text
 
 
-# for i in range(1,10):
-#     try:
-#         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-#         sleep(5)
-#         driver.find_element_by_link_text(str(i)).click()
-#         list_product = driver.find_elements_by_class_name("ThingCard__thingCard--1IcHY")
-#         print(len(list_product))
-#         for i in range(len(list_product)):
-#             try:
-#                 row_text = list_product[i].click()
-#                 sleep(5)
-#                 driver.execute_script("window.history.go(-1)")
-#                 sleep(3)
-#                 driver.find_element_by_xpath(
-#                     "//span[text()='3D Printing']").click()
-#                 sleep(7)
-#             except Exception as e :
-#                 print(e)
+        list_of_products.append([name,owner,summary])
+    except :
+        pass
+
+print(list_of_products[:3])
+
+
+product_json = {}
+
+# for product in list_of_products:
+#     product_json({'name': product.__getattribute__(name), 'owner': product.__getattribute__(owner) , 'summary': product.__getattribute__(summary)})
 #
 #
 #
 #
-#         all_products+=list_product
-#         sleep(7)
-#     except:
-#         pass
+# with open('product.json', 'w') as json_file:
+#     json.dump(product_json, json_file)
 #
