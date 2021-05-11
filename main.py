9a554b024bdf47b1962553d3fea9062d6fe3d9e6
zaher-abb <abboudz@th-brandenburg.de>
2021-05-11 13:24:50 +0200

    saving data in json format

--- main.py
+++ main.py
@@ -1,56 +1,71 @@
 from selenium import webdriver
 from time import sleep
 import pandas as pd
+import csv
+import json
 
-
-class Product:
-
-    # TODO : persons who write comments and the comments
-    def __init__(self, name, owner, summary):
-        self.owner = owner
-        self.name = name
-        self.summary = summary
-
+# Chrom  Driver
 
 driver = webdriver.Chrome("D:\ChromDriver\chromedriver_win32\chromedriver.exe")
 
-driver.get('https://www.thingiverse.com/')
-sleep(3)
-
-categorySort__dropdown = driver.find_element_by_xpath(
-    "//button[contains(@class,'CategorySort__dropdownButton--gpHIi Dropdown__dropdownButton--1iEp1')]").click()
-categorySort__dropdown_select_3d_Printing = driver.find_element_by_xpath("//span[text()='3D Printing']").click()
-sleep(3)
+#
+# # Home Page
+# driver.get('https://www.thingiverse.com/')
+# sleep(3)
+# # chose the 3d printing
+# categorySort__dropdown = driver.find_element_by_xpath(
+#     "//button[contains(@class,'CategorySort__dropdownButton--gpHIi Dropdown__dropdownButton--1iEp1')]").click()
+# categorySort__dropdown_select_3d_Printing = driver.find_element_by_xpath("//span[text()='3D Printing']").click()
+# sleep(3)
+#
+#
+# url_list=[]
+# condition = True
+# testTemp = 0
+# temp=1
+# while condition:
+#
+#     result = driver.find_elements_by_class_name('ThingCardBody__cardBodyWrapper--ba5pu')
+#     temp += 1
+#     print(temp)
+#     for i in range(len(result)):
+#         try:
+#             # print(i)
+#             url_list.append(result[i].get_property('href'))
+#
+#
+#         except Exception as  e:
+#             print(e)
+#     try:
+#         driver.find_element_by_xpath("//div[@class='Pagination__button--2X-2z Pagination__more--24exV']").click()
+#
+#         # testTemp+=1
+#         # if testTemp >=1 :
+#         #     break
+#         sleep(5)
+#     except Exception as e:
+#         condition = False
+#         print(e)
 
 url_list = []
-
-condition = True
-testTemp = 0
-while condition:
-
-    result = driver.find_elements_by_class_name('ThingCardBody__cardBodyWrapper--ba5pu')
-
-    for i in range(len(result)):
-        try:
-            url_list.append(result[i].get_property('href'))
-        except Exception as  e:
-            print(e)
-    try:
-        driver.find_element_by_xpath("//div[@class='Pagination__button--2X-2z Pagination__more--24exV']").click()
-
-        # testTemp+=1
-        # if testTemp >=1 :
-        #     break
-        sleep(5)
-    except Exception as e:
-        condition = False
-        print(e)
-
+# save all products urls from 3d printing in csv file
+with open('Products_urls.csv','rt')as f:
+  data = csv.reader(f)
+  for row in data:
+       url_list+=row
 print(len(url_list))
+#
+
+# df2=pd.DataFrame(url_list)
+# df2.to_csv('Products_urls.csv', index=False, header=False)
+
+driver.get('https://www.thingiverse.com/')
 users_url_who_writes_comments = []
 list_of_products = []
-for i in url_list:
+followers_list=[]
+for i in list(url_list[:3]) :
     try:
+        print(i)
         driver.get(i)
         sleep(5)
         # name
@@ -66,15 +81,46 @@ for i in url_list:
         sleep(4)
         list_of_comments = driver.find_elements_by_class_name('ThingComment__modelName--Vqvbz')
 
+
+        # create list of comments from a product
         for i in list_of_comments:
             users_url_who_writes_comments.append(i.get_property('href'))
+        sleep(3)
 
-        list_of_products.append([name, owner, summary,users_url_who_writes_comments])
+        owner_profile = driver.find_element_by_xpath(
+            "//div[@class='ThingPage__gallery--SnBAG']//div[2]/a").get_attribute('href')
+        sleep(1)
+
+        #  get into the owner Profile
+        driver.get(owner_profile)
+
+
+        sleep(3)
+        # get list of urls of followers
+        get_followers_url = driver.find_element_by_xpath(
+            '//*[@id="react-app"]/div/div/div/div[4]/div[1]/div[1]/div[3]/a[1]').get_attribute('href')
+        sleep(1)
+        driver.get(get_followers_url)
+        get_all_followers = driver.find_elements_by_class_name('user-header')
+        for i in get_all_followers :
+            followers_list.append(i.get_attribute('href'))
+
+        list_of_products.append((name, owner, summary,users_url_who_writes_comments,followers_list))
     except:
         pass
 
 
+df = pd.DataFrame(data=list_of_products,columns=['Product_name','owner','summary','users_Profile_who_writes_comments','followers'])
 
-df = pd.DataFrame(list_of_products)
-df.to_csv('3d_printer_Data.csv', index=False, header=False)
 
+# storing data in JSON format
+df.to_json('file1.json', orient='index')
+
+# reading the JSON file
+df = pd.read_json('file1.json', orient='index')
+# Pretty Printing JSON string back
+
+
+# pd.set_option('display.max_columns',5)
+# pd.set_option("display.max_colwidth", None)
+# print(df)
